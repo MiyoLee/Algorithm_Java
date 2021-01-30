deefb3629322a2557748c61eb63d8a16585252ed
MiyoLee <62459690+MiyoLee@users.noreply.github.com>
2021-01-30 20:43:23 +0900

    Delete ssafy directory

--- ssafy/hw08/DuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.hw08;
-
-public class DuplicateException extends Exception{
-	
-	public DuplicateException() {
-		super("이미 존재하는 상품입니다.");
-	}
-}
--- ssafy/hw08/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.hw08;
-
-public class ProductNotFoundException extends Exception {
-	public ProductNotFoundException() {
-		super("상품이 존재하지 않습니다.");
-	}
-}
