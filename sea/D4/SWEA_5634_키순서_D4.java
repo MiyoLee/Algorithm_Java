953e0b4f13940f3737ea67b01fd6d308cd0377c7
MiyoLee <62459690+MiyoLee@users.noreply.github.com>
2021-04-21 17:22:50 +0900

    Add files via upload

---
+++ sea/D4/SWEA_5634_키순서_D4.java
@@ -0,0 +1,70 @@
+package com.ssafy.hwalgo31;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class SWEA_5634_키순서_D4 {
+	static int N,M;
+	static boolean[][] adj;
+	static int answer;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			M = Integer.parseInt(br.readLine());
+			adj = new boolean[N+1][N+1];
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				adj[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]=true;
+			}
+			//입력 완료. 1->2 : 1보다 2가 크다
+			//자신의 키가 몇번째인지 알 수 있는 학생? 나보다 작은 사람 + 나보다 큰사람 = N-1일때.
+			answer=0;
+			for (int i = 1; i <= N; i++) {
+				if(taller(i,new boolean[N+1]) + shorter(i,new boolean[N+1]) == N-1) ++answer;
+			}
+			System.out.println("#"+tc+" "+answer);
+		}
+	}
+	
+	// 자기 자신보다 큰 사람이 몇명인지 리턴하는 함수
+	private static int taller(int n, boolean[] visited) {
+		int result=0;
+		Queue<Integer>q = new LinkedList<>();
+		q.offer(n);
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			for (int j = 1; j <= N; j++) {
+				if(!visited[j] && adj[cur][j]) {
+					visited[j]=true;
+					++result;
+					q.offer(j);
+				}
+			}
+		}
+		return result;
+	}
+	// 자기 자신보다 작은 사람이 몇명인지 리턴하는 함수
+	private static int shorter(int n, boolean[] visited) {
+		int result=0;
+		Queue<Integer>q = new LinkedList<>();
+		q.offer(n);
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			for (int i = 1; i <= N; i++) {
+				if(!visited[i] && adj[i][cur]) {
+					visited[i]=true;
+					++result;
+					q.offer(i);
+				}
+			}
+		}
+		return result;
+	}
+}
