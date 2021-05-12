d36d6d8b1b6003551bc20b9be96bf4929816cb3c
MiyoLee <62459690+MiyoLee@users.noreply.github.com>
2021-05-12 17:02:19 +0900

    Add files via upload
    이걸로 보기!!!!!! 따라해보기

--- BOJ/BOJ_15683_감시_G5.java
+++ BOJ/BOJ_15683_감시_G5.java
@@ -3,172 +3,115 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_15683_감시_G5 {
-	static int N, M;
-	static int[][] map;
-	static List<Pos> cctvs;
-	static int[] dr = { -1, 0, 1, 0 };	//상우하좌
-	static int[] dc = { 0, 1, 0, -1 };
-	static int answer;
+	static class Cctv {
+		int r, c, cctv;
 
-	static class Pos {
-		int r, c;
-		int num;
-
-		public Pos(int r, int c, int num) {
+		public Cctv(int r, int c, int cctv) {
 			super();
 			this.r = r;
 			this.c = c;
-			this.num = num;
+			this.cctv = cctv;
 		}
 	}
 
+
+	static int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    static int[][] dir = {
+            {},
+            {1},
+            {1, 3},
+            {0, 1},
+            {0, 1, 3},
+            {0, 1, 2, 3}
+    };
+	static int n, m, cnt;
+	static Cctv[] tv = new Cctv[8];
+	static int min = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); // 세로크기
-		M = Integer.parseInt(st.nextToken()); // 가로크기
-		map = new int[N][M];
-		cctvs = new ArrayList<>(); // cctv의 좌표와 번호 저장할 배열
-
-		for (int i = 0; i < N; i++) {
+		//br = new BufferedReader(new StringReader(src));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int[][] map = new int[n][m];
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
+			for (int j = 0; j < m; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] >= 1 && map[i][j] <= 5)
-					cctvs.add(new Pos(i, j, map[i][j]));
 			}
 		}
-		// 입력 완료.
-		answer = Integer.MAX_VALUE;
-		dfs(0, map);
-		System.out.println(answer);
-	}
-
-	private static boolean dfs(int index, int[][] map) { // 현재 몇번째 cctv 인지, 현재 맵 상황
-		int zero = getZeros(map);
-		if(zero==0) {
-			answer=0;
-			return true;
-		}
-		if (index == cctvs.size()) { // 모든 cctv다 검사 했다.
-			answer = Math.min(getZeros(map), answer);
-			return false;
-		}
-
-		Pos cur = cctvs.get(index); // 현재 cctv의 정보
-		int dirCase = 0;
-		switch (cur.num) {
-		case 1:
-			dirCase = 4;
-			break;
-		case 2:
-			dirCase = 2;
-			break;
-		case 3:
-			dirCase = 4;
-			break;
-		case 4:
-			dirCase = 4;
-			break;
-		case 5:
-			dirCase = 1;
-			break;
-		}
-		for (int dirN = 0; dirN < dirCase; dirN++) {
-			int[][] newMap = new int[N][M];
-			copy(newMap, map);
-
-			bfs(cur, dirN, newMap);
-			if(dfs(index + 1, newMap)) return true;
-			// 리턴하면 map도 원래대로 돌아간다.
-			
-		}
-		return false;
-	}
-
-	// newMap에 map을 복사하는 함수
-	private static void copy(int[][] newMap, int[][] map) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				newMap[i][j] = map[i][j];
-			}
-		}
-	}
-
-	private static void bfs(Pos cur, int dirN, int[][] map) {
-		if (cur.num == 5) {
-			for (int d = 0; d < 4; d++) {
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				while (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != 6) {
-					if(map[nr][nc]==0) map[nr][nc] = -1;
-					nr += dr[d];
-					nc += dc[d];
-				}
-			}
-		} else if (cur.num == 1) {
-			int nr = cur.r + dr[dirN];
-			int nc = cur.c + dc[dirN];
-			while (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != 6) {
-				if(map[nr][nc]==0) map[nr][nc] = -1;
-				nr += dr[dirN];
-				nc += dc[dirN];
-			}
-		} else if (cur.num == 2) {
-			// dirN:0 -> 상하, dirN:1 -> 좌우
-			for (int d = dirN; d <= dirN+2; d+=2) {
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				while (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != 6) {
-					if(map[nr][nc]==0) map[nr][nc] = -1;
-					nr += dr[d];
-					nc += dc[d];
-				}
-			}
-		} else if (cur.num == 3) { // 직각으로 두방향
-			// dirN:0 -> 상우, dirN:1 -> 하좌, dirN:2 -> 좌상, dirN:3 -> 우하
-			for (int d = dirN; d <= dirN+1; d++) {
-				d%=4;
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				while (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != 6) {
-					if(map[nr][nc]==0) map[nr][nc] = -1;
-					nr += dr[d];
-					nc += dc[d];
-				}
-			}
-		} else if (cur.num == 4) { // 직각으로 두방향
-			for (int d = 0; d < 4; d++) {
-				if (d == dirN)
-					continue;
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				while (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] != 6) {
-					if(map[nr][nc]==0) map[nr][nc] = -1;
-					nr += dr[d];
-					nc += dc[d];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] > 0 && map[i][j] < 6) {
+					tv[cnt++] = new Cctv(i, j, map[i][j]);
 				}
 			}
 		}
+		
+		DFS(0, map);
+		System.out.println(min);
 	}
 
-	// map에서 0의 갯수 구하는 함수
-	private static int getZeros(int[][] map) {
-		int cnt = 0;
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				if (map[i][j] == 0)
-					++cnt;
+	static void DFS(int idx, int[][] map) {
+		if (idx == cnt) {
+//			for(int i =0 ;i<n; i++) {
+//				for(int j =0; j<m; j++) {
+//					System.out.print(map[i][j]+" ");
+//				}
+//				System.out.println();
+//			}
+//			System.out.println();
+			int count=0;
+			for(int i =0 ;i<n; i++) {
+				for(int j =0; j<m; j++) {
+					if(map[i][j]==0)
+						count++;
+				}	
+			}
+			min = Math.min(min, count);
+			return;
+		}
+		Cctv top = tv[idx];
+		for (int d = 0; d < 4; d++) {
+			int[][] copymap = copy(map);
+			for (int i = 0; i < dir[top.cctv].length; i++) {
+				int newR = top.r;
+				int newC = top.c;
+				int newD = (dir[top.cctv][i]-d+3)%4;   /////////////
+				
+				while(true) {
+					newR +=dirs[newD][0];
+					newC +=dirs[newD][1];
+					//break 걸어야함 , 맵 나갔을때, 6만났을떄
+					if(!isRange(newR, newC) || map[newR][newC]==6) break;
+					copymap[newR][newC] = 9;
+				}
+			}
+			DFS(idx+1, copymap);
+		}
+	}
+	static boolean isRange(int r, int c) {
+		return r>=0 && c>=0 && n>r && m>c;
+	}
+
+	static int[][] copy(int[][] map) {
+		int[][] copymap = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <m; j++) {
+				copymap[i][j] = map[i][j];
 			}
 		}
-		return cnt;
+		return copymap;
 	}
 
+	static String src = "4 5\r\n" + 
+			"0 0 0 6 0\r\n" + 
+			"6 0 6 0 0 \r\n" + 
+			"2 6 0 0 0\r\n" + 
+			"0 0 6 0 3";
 }
